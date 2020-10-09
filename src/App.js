6efd63920078c17a6c0b97b9b2c39bab448088ca
Trey <cooper6101@gmail.com>
2020-10-09 17:27:16 -0400

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,112 +1,45 @@
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
+import Footer from './components/layout/Footer';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import About from './components/pages/About';
-import axios from 'axios';
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
 import './App.css';
 
 const App = () => {
 
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-
-  // Search Github Users
-  const searchUsers = async text => {
-
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
-    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUsers(res.data.items);
-    setLoading(false);
-  }
-
-  // Get single github user
-  const getUser = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(`https://api.github.com/users/${username}?client_id=
-    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setUser(res.data);
-    setLoading(false);
-  }
-
-  const getUserRepos = async (username) => {
-    setLoading(true);
-
-    const res = await axios.get(
-    `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
-    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    setRepos(res.data);
-    setLoading(false);
-  }
-
-  //Clear Users from State
-  const clearUsers = () => {
-    setUsers([]);
-    setLoading(false);
-  };
-
-  //Set alert for no text
-  const showAlert = (msg, type) => {
-    setAlert({msg, type });
-
-    setTimeout(() => setAlert(null), 5000);
-  }
-
     return (
-      <Router>
-          <div className="App">
+      <GithubState>
+        <AlertState>
+            <Router>
+                <div className="App">
 
-          <Navbar />
+                <Navbar />
 
-          <div className='container'>
-            <Alert alert={alert} />
+                <div className='container' style={{ minHeight: '500px' }}>
+                  <Alert />
 
-            <Switch>
-              <Route exact path='/' render={props => (
-                <Fragment>
-                  <Search
-                    searchUsers={searchUsers} 
-                    clearUsers={clearUsers} 
-                    showClear={ users.length > 0 ? true : false} 
-                    setAlert={showAlert}
-                  />
-                  <Users loading={ loading } users={ users } />
-                </Fragment>
-              )} />
+                  <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route exact path='/about' component={About} />
+                    <Route exact path='/user/:login' component={User} />
+                    <Route component={NotFound} />
+                  </Switch>
+                </div>
 
-              <Route exact path='/about' component={About} />
-
-              <Route exact path='/user/:login' render={props => (
-                <User 
-                { ...props } 
-                getUser={getUser} 
-                getUserRepos={getUserRepos}
-                user={user} 
-                repos={repos}
-                loading={loading} 
-                />
-              )} />
-            </Switch>
-          </div>
-
-          </div>
-      </Router>
+                </div>
+                <Footer />
+            </Router>
+        </AlertState>
+      </GithubState>
 
     );
 
